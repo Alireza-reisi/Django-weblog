965cf1fbbd66aa5f5f68dbdc038c52357abd178f
alireza reisi <alireza.reisi03127@gmail.com>
2024-04-19 15:10:55 +0430

    create and setup login,logout and signup page

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('login/', views.login),
+    path('login/', views.users_login),
+    path('signup/', views.users_signup),
+    path('logout/', views.users_logout),
+
 ]
--- account/views.py
+++ account/views.py
@@ -1,7 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 
 # Create your views here.
 
-def login(request):
+def users_login(request):
+    if request.method == 'POST':
+        user = authenticate(request, username=request.POST.get('username'),
+                            password=request.POST.get('password'))
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+
     return render(request, 'login.html')
+
+
+def users_signup(request):
+    if request.method == 'POST':
+        user = User.objects.create_user(username=request.POST.get('username'), email=request.POST.get('gmail'),
+                                        password=request.POST.get('password'))
+        login(request, user)
+        return redirect('/')
+    return render(request, 'signup.html')
+
+
+def users_logout(request):
+    logout(request)
+    return redirect('/')
