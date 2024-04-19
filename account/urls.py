67425c0e041b381435d25d99685a814b55fa79c8
alireza reisi <alireza.reisi03127@gmail.com>
2024-04-19 15:45:15 +0430

    make links dinamic

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+
+
+app_name = 'account'
 urlpatterns = [
-    path('login/', views.users_login),
-    path('signup/', views.users_signup),
-    path('logout/', views.users_logout),
+    path('login/', views.users_login, name='login'),
+    path('signup/', views.users_signup, name='signup'),
+    path('logout/', views.users_logout, name='logout'),
 
 ]
--- account/views.py
+++ account/views.py
@@ -6,25 +6,34 @@ from django.contrib.auth.models import User
 # Create your views here.
 
 def users_login(request):
-    if request.method == 'POST':
+    if request.user.is_authenticated:
+        return redirect('home:home')
+    elif request.method == 'POST':
         user = authenticate(request, username=request.POST.get('username'),
                             password=request.POST.get('password'))
         if user is not None:
             login(request, user)
-            return redirect('/')
+            return redirect('home:home')
 
     return render(request, 'login.html')
 
 
 def users_signup(request):
-    if request.method == 'POST':
-        user = User.objects.create_user(username=request.POST.get('username'), email=request.POST.get('gmail'),
-                                        password=request.POST.get('password'))
+    if request.user.is_authenticated:
+        return redirect('home:home')
+    elif request.method == 'POST':
+        user = authenticate(request, username=request.POST.get('username'),
+                            password=request.POST.get('password'))
+        if user is not None:
+            return redirect('account:login')
+        else:
+            user = User.objects.create_user(username=request.POST.get('username'), email=request.POST.get('gmail'),
+                                            password=request.POST.get('password'))
         login(request, user)
-        return redirect('/')
+        return redirect('home:home')
     return render(request, 'signup.html')
 
 
 def users_logout(request):
     logout(request)
-    return redirect('/')
+    return redirect('home:home')
